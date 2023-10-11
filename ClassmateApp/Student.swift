04b85cbe8abec7a8c611462dcc88438454abbdd2
Mason Bartell <masonjbartell@gmail.com>
2023-10-11 09:43:44 -0500

    k

---
+++ ClassmateApp/Student.swift
@@ -0,0 +1,22 @@
+//
+//  Student.swift
+//  ClassmateApp
+//
+//  Created by MASON BARTELL on 10/11/23.
+//
+
+import Foundation
+
+
+
+public struct Student {
+    
+    var name : String
+    var age : Int
+    var grade : Int
+    
+    
+    
+    
+    
+}
--- ClassmateApp/ViewController.swift
+++ ClassmateApp/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var studentArray : [Student] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        var s1 = Student(name: "Jake", age: 17, grade: 12)
+        var s2 = Student(name: "Tyler", age: 17, grade: 12)
+        var s3 = Student(name: "Oliver", age: 26, grade: 12)
+        var s4 = Student(name: "Noel", age: 16, grade: 11)
+        studentArray.append(s1)
+        
+        
     }
-
+    
+    
+    
+    
 
 }
 
